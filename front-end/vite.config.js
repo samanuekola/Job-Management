2454827867cbc45be868a5e55cfec5e84428084a
samanuekola <samamanue@gmail.com>
2024-09-02 15:37:04 +0530

    deployment

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,14 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Job-Management/', // Replace with your repository name
+  base: 'Job-Management', // Replace with your repository name
 })
